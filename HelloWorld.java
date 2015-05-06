1db74d5afd1debbc6e7ee075b9060ef87ffd62c2
Vailthor <nloveless96@live.com>
2015-05-06 08:54:41 -0700

    We will rock you!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
+
 public class HelloWorld
 {
+  private static Scanner in = new Scanner(System.in);
   public static void main(String[] args)
   {
-    System.out.println("Hello World!");
     System.out.println("We will rock you!");
+    System.out.println("If you want to rock with us tell me your name! ");
+    String name = in.nextLine();
+    System.out.println("Welcome " + name + "!");
   }
 }
